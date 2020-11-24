0be8fadaa04c2188de961e246f7e00fff5992e69
git <raspberry>
2020-11-25 00:50:25 +0400

    Понеслось)

---
+++ backend/main.py
@@ -0,0 +1,28 @@
+import aiohttp_cors as aiohttp_cors
+from aiohttp import web
+
+
+routes = web.RouteTableDef()
+
+
+@routes.get('/')
+async def hello(request):
+    return web.Response(text='Welcome to chipicao caps')
+
+
+if __name__ == '__main__':
+    app = web.Application()
+    app.add_routes(routes)
+
+    cors = aiohttp_cors.setup(app, defaults={
+        "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+        )
+    })
+
+    for route in list(app.router.routes()):
+        cors.add(route)
+
+    web.run_app(app, host='127.0.0.1', port=5000)
