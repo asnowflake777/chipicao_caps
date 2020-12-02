93ca1756edbe052bbb2f96ede69aca2a2a2c14c3
git <raspberry>
2020-12-02 22:57:49 +0400

    Добавил сериализаторы для остальных моделей

--- backend/db/models.py
+++ backend/db/models.py
@@ -24,6 +24,17 @@ class Series(models.Model):
     creator = fields.ForeignKeyField('models.User', related_name='series')
     image_uid = fields.CharField(max_length=64)
 
+    async def serialize(self):
+        creator = await self.creator.first()
+        creator = await creator.serialize()
+        return {
+            'id': self.pk,
+            'name': self.name,
+            'description': self.description,
+            'creator': creator,
+            'image_uid': self.image_uid,
+        }
+
 
 class SeriesItem(models.Model):
     name = fields.CharField(max_length=64)
@@ -31,10 +42,31 @@ class SeriesItem(models.Model):
     identify_number = fields.IntField()
     series = fields.ForeignKeyField('models.Series', related_name='items')
 
+    async def serialize(self):
+        series = await self.series.first()
+        series = await series.serialize()
+        return {
+            'id': self.pk,
+            'name': self.name,
+            'description': self.description,
+            'identify_number': self.identify_number,
+            'series': series,
+        }
+
 
 class UserItemLink(models.Model):
     user = fields.ForeignKeyField('models.User', related_name='items')
     item = fields.ForeignKeyField('models.SeriesItem', related_name='users')
+
+    async def serialize(self):
+        user = await self.user.first()
+        user = await user.serialize()
+        item = await self.item.first()
+        item = await item.serialize()
+        return {
+            'user': user,
+            'item': item,
+        }
 
 
 MODEL_MAPPER = {
